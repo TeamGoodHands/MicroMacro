02bec83cff29bc4d334f0069a146c7aee08c5a88
harumas <haruto09.game@gmail.com>
2025-07-08 15:56:33 +0900

    wip

---
+++ MicroMacro/Assets/Scripts/Module/Management/LevelUnloader.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace Module.Management
+{
+    public class LevelUnloader : MonoBehaviour
+    {
+        private void Update()
+        {
+        }
+    }
+}
