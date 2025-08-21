dbca72759061c48c4a4d40c7f099c55eb410b313
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-08-22 08:56:03 +0900

    微修正

--- MicroMacro/Assets/Scripts/Module/Application/SceneSwitch/TempQuitGame.cs
+++ MicroMacro/Assets/Scripts/Module/Application/SceneSwitch/TempQuitGame.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 using UnityEngine;
 
-namespace Module.Application.SceneSwitch
+public class TempQuitGame : MonoBehaviour
 {
-    public class TempQuitGame : MonoBehaviour
+    // 簡易終了処理
+    private void Update()
     {
-        // 簡易終了処理
-        private void Update()
+        if (Input.GetKey(KeyCode.Escape))
         {
-            if (Input.GetKey(KeyCode.Escape))
-            {
 
 #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false; //ゲームプレイ終了
+            UnityEditor.EditorApplication.isPlaying = false; //ゲームプレイ終了
 #else
     Application.Quit();
 #endif
-            }
         }
     }
 }
