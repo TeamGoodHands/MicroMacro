cf873624629e4bc4155445362be64993eb89c6d8
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-08-29 05:04:30 +0900

    重さによる落下ギミック仮実装

---
+++ MicroMacro/Assets/Scripts/Module/Gimmick/RopeLine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Module.Gimmick
+{
+    public class RopeLine : MonoBehaviour
+    {
+        [SerializeField] LineRenderer lineRenderer;
+        [SerializeField] Transform startPoint;
+        [SerializeField] Transform endPoint;
+
+        void Update()
+        {
+            Vector3[] positions = new Vector3[] { startPoint.position, endPoint.position, };
+            lineRenderer?.SetPositions(positions);
+        }
+    }
+}
