5ec8e87901edfa335ebfca47ba0e7598f7187fbc
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-08-29 00:17:51 +0900

    1-1と1-2の仮統合とprefab等の修正と調整

---
+++ MicroMacro/Assets/Scripts/Module/Gimmick/TPGate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Constants;
+using UnityEngine;
+
+namespace Module.Gimmick
+{
+    public class TPGate : MonoBehaviour
+    {
+        [Header("移動先")] 
+        [SerializeField] private Transform destination;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(Tag.Handle.Player))
+            {
+                other.transform.position = destination.position;
+            }
+        }
+    }
+}
