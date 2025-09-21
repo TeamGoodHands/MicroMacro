8f93ee30c9c74931ccdd12a96a86095e053d2276
harumas <haruto09.game@gmail.com>
2025-09-21 20:08:25 +0900

    エリアカメラ実装

---
+++ MicroMacro/Assets/Scripts/Module/Level/PlayerComponentSearcher.cs
@@ -0,0 +1,28 @@
+using Constants;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Module.Level
+{
+#if UNITY_EDITOR
+    [ExecuteInEditMode]
+    public class PlayerComponentSearcher : MonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera cinemachineCamera;
+
+
+        private void OnValidate()
+        {
+            // すでにターゲットが設定されている場合は何もしない
+            if (cinemachineCamera.Target.TrackingTarget != null)
+                return;
+                
+            GameObject player = GameObject.FindWithTag(Tag.Player);
+            if (player == null)
+                return;
+
+            cinemachineCamera.Target.TrackingTarget = player.transform;
+        }
+    }
+#endif
+}
