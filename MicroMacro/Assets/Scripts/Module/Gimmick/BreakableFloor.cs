f17f9250a6e41766c1bb34069952e3cb84ff4156
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-10-01 04:10:14 +0900

    壊れる床実装

---
+++ MicroMacro/Assets/Scripts/Module/Gimmick/BreakableFloor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Module.Player.Component;
+using UnityEngine;
+
+namespace Module.Gimmick
+{
+    public class BreakableFloor : MonoBehaviour
+    {
+        [Header("床を破壊するオブジェクト")]
+        [SerializeField] private GameObject triggerObj;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject == triggerObj)
+            {
+                Break();
+            }
+        }
+
+        private void Break()
+        {
+            Destroy(triggerObj);
+            Destroy(gameObject);
+        }
+    }
+}
