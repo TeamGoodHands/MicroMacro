28ad2f42f63d209a336fa8935f2a337084d6e3aa
harumas <haruto09.game@gmail.com>
2025-11-08 17:52:06 +0900

    オープニングムービー追加

---
+++ MicroMacro/Assets/Scripts/Module/UI/SceneButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Module.UI
+{
+    public class SceneButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private string sceneName;
+        
+        public string SceneName => sceneName;
+        public Button Button => button;
+    }
+}
