2133ad56036627379bda599bc3b787958bf863bc
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2025-11-26 12:06:41 +0900

    wip

--- MicroMacro/Assets/Scripts/Module/Gimmick/OpenGateEvent.cs
+++ MicroMacro/Assets/Scripts/Module/Gimmick/OpenGateEvent.cs
@@ -12,6 +12,8 @@ namespace Module.Gimmick
     {
         [SerializeField] private GameObject gate;
         [SerializeField] private PlayableDirector director;
+        [Header("ステージ上に配置する木の実")][SerializeField] private GameObject fallNuts;
+        [Header("虫に付随し食べられる木の実")][SerializeField] private GameObject nutsForEat;
 
         private PlayerBehaviour playerBehaviour;
         private bool isTriggered = false;
@@ -19,6 +21,10 @@ namespace Module.Gimmick
         private void Start()
         {
             playerBehaviour = GameObject.FindWithTag(Tag.Player).GetComponent<PlayerBehaviour>();
+            if (nutsForEat != null && nutsForEat.activeSelf)
+            {
+                nutsForEat.SetActive(false);
+            }
         }
 
         private void OnCollisionEnter(Collision other)
@@ -28,24 +34,41 @@ namespace Module.Gimmick
             
             if (other.gameObject.CompareTag(Tag.Handle.Untagged))
             {
-                OpenGate().Forget();
+                EatNuts().Forget();
                 isTriggered = true;
                 SoundManager.instance.Play("スイッチ");
             }
         }
 
-        private async UniTaskVoid OpenGate()
+        private async UniTaskVoid EatNuts()
         {
+            if (nutsForEat == null)
+                return;
+            
             playerBehaviour.Component.Condition.IsPlayerLocked = true;
+            ChangeActiveNuts();
+            // TODO : 木の実を食べるアニメーション再生
+            await UniTask.Delay(TimeSpan.FromSeconds(3));  
+            
+            nutsForEat.SetActive(false);
+            DoorBreak().Forget();
+        }
+
+        private async UniTaskVoid DoorBreak()
+        {
             director.Play();
-            
             await UniTask.Delay(TimeSpan.FromSeconds(3));
-            
+            // TODO: ドア破壊アニメーション再生
             gate.SetActive(false);
             
             await UniTask.Delay(TimeSpan.FromSeconds(2));
-            
             playerBehaviour.Component.Condition.IsPlayerLocked = false;
+        }
+
+        private void ChangeActiveNuts()
+        {
+            fallNuts.SetActive(false);
+            nutsForEat.SetActive(true);
         }
     }
 }
