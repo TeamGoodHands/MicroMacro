62610af80767f5965a88ec259357084c5cd51159
harumas <haruto09.game@gmail.com>
2025-12-10 13:45:56 +0900

    wip

--- MicroMacro/Assets/Scripts/Module/Scaling/ObjectSpawner.cs
+++ MicroMacro/Assets/Scripts/Module/Scaling/ObjectSpawner.cs
@@ -1,18 +1,22 @@
 using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
     [SerializeField] private float interval;
+    [SerializeField] private float firstDelay;
 
     private async void Start()
     {
+        await UniTask.Delay(TimeSpan.FromSeconds(firstDelay), cancellationToken: destroyCancellationToken);
+
         while (!destroyCancellationToken.IsCancellationRequested)
         {
             Instantiate(prefab, transform);
-            await UniTask.Delay(TimeSpan.FromSeconds(interval));
+            await UniTask.Delay(TimeSpan.FromSeconds(interval), cancellationToken: destroyCancellationToken);
         }
     }
 }
