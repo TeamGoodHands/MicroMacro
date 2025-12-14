6e55dbe0f40ec0f0c1540c82c1f10d924bb669e2
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-12-15 04:28:34 +0900

    エラーログのドライブアップロードとプレイ動画との紐づけ

--- MicroMacro/Assets/Scripts/Module/Application/ErrorLogger.cs
+++ MicroMacro/Assets/Scripts/Module/Application/ErrorLogger.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.IO; 
 using System.Text; 
+using UnityEngine.Networking;
+using Cysharp.Threading.Tasks;
+using Module.Application.Record;
 
 public class ErrorLogger : MonoBehaviour
 {
@@ -8,14 +12,18 @@ public class ErrorLogger : MonoBehaviour
 
     // trueにするとエラーと例外のみ、falseにすると全ログを記録
     [SerializeField]
-    private bool logErrorsOnly = true; 
+    private bool logErrorsOnly = true;
+
+    // ドライブに送信する用GASのURL
+    [SerializeField] private string gasWebAppUrl =
+        "https://script.google.com/macros/s/AKfycby-_KHHNDwTfiuUwysIeUm6JpSveyf5pHEwFkVjOptdhYfJI5hyfxvBGhfEYClcFZJh/exec";
 
     private string logFilePath;
+    // StringBuilder : 文字列の編集ができるstring
     private StringBuilder logBuilder = new StringBuilder();
 
     void Awake()
     {
-        // シンプルなシングルトン設定
         if (Instance == null)
         {
             Instance = this;
@@ -23,8 +31,6 @@ public class ErrorLogger : MonoBehaviour
             
             // ログファイルのパスを決定
             logFilePath = Path.Combine(Application.persistentDataPath, "game_log.txt");
-            
-            // 起動時にログファイルの場所をコンソールに出しておく
             Debug.Log($"ログファイル保存先: {logFilePath}");
         }
         else
@@ -35,13 +41,11 @@ public class ErrorLogger : MonoBehaviour
 
     void OnEnable()
     {
-        // イベントにメソッドを登録
         Application.logMessageReceived += HandleLog;
     }
 
     void OnDisable()
     {
-        // オブジェクトが破棄される際にイベントから登録解除
         Application.logMessageReceived -= HandleLog;
     }
 
@@ -61,8 +65,9 @@ public class ErrorLogger : MonoBehaviour
         logBuilder.AppendLine(stackTrace);
         logBuilder.AppendLine("--------------------");
 
-        // ファイルに追記
-        // (try-catchで囲むと、ファイルI/Oエラーで無限ループするのを防げる)
+        string finalLog = logBuilder.ToString();
+        
+        // ローカル保存
         try
         {
             File.AppendAllText(logFilePath, logBuilder.ToString());
@@ -72,5 +77,70 @@ public class ErrorLogger : MonoBehaviour
             // ここでのDebug.Logは無限ループするので避ける
             Debug.LogWarning($"ログの書き込みに失敗: {ex.Message}");
         }
+        
+        // Driveに送信
+        UploadLogToDriveAsync(finalLog).Forget();
+    }
+
+    private async UniTaskVoid UploadLogToDriveAsync(string logContent)
+    {
+        // 回線繋がってない場合弾く
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+            return;
+
+        // ID取得
+        string playId = "unknown_id";
+        if (RecordManager.Instance != null && !string.IsNullOrEmpty(RecordManager.Instance.CurrentTestPlayID))
+        {
+            playId = RecordManager.Instance.CurrentTestPlayID;
+        }
+
+        // ファイル名にIDを含める (GAS側での紐づけ用)
+        string fileName = $"ErrorLog_{playId}_{System.DateTime.Now:yyyyMMMMdd_HHmmss}.txt";
+        
+        // Jsonデータ作成
+        LogData data = new LogData
+        {
+            fileName = fileName,
+            content = logContent
+        };
+        string json = JsonUtility.ToJson(data);
+
+        // POST送信
+        using (var www = new UnityWebRequest(gasWebAppUrl, "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+            www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            try
+            {
+                await www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogWarning($"ログアップロード失敗: {www.error}");
+                }
+                else
+                {
+                    // 成功ログ出したって良い
+                    Debug.Log("ログアップロード完了");
+                }
+            }
+            catch (Exception ex)
+            {
+                // キャンセルやその他の通信エラー
+                Debug.LogWarning($"ログ送信例外: {ex.Message}");
+            }
+        }
+
+    }
+
+    [Serializable]
+    private class LogData
+    {
+        public string fileName;
+        public string content;
     }
 }
