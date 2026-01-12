3b0f4acebb101f9cd3b36a4ae4c24e0d58e59403
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2026-01-12 12:47:24 +0900

    新セリフ表示妥協の実装

--- MicroMacro/Assets/Scripts/Module/Application/Dialogue/DialogueUI.cs
+++ MicroMacro/Assets/Scripts/Module/Application/Dialogue/DialogueUI.cs
@@ -5,25 +5,30 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
-
 namespace Module.Application.Dialogue
 {
      public class DialogueUI : MonoBehaviour
     {
-        [SerializeField] private GameObject dialogueWindow;
-        [SerializeField] private Image characterIcon;
-        [SerializeField] private TextMeshProUGUI dialogueText;
-        [SerializeField] private GameObject dialogueTextObject;
+        [SerializeField] private Image fuguSpeechBubble;
+        [SerializeField] private Image playerSpeechBubble;
+        [SerializeField] private TextMeshProUGUI fuguText;
+        [SerializeField] private TextMeshProUGUI playerText;
         [Header("ウィンドウ表示、非表示にかかる時間")][SerializeField] private float playBackTime = 0.3f;
         [SerializeField] [Range(0f, 1f)] private float maxSize;
         
+        private TextMeshProUGUI activeText;
+        private Image activeSpeechBubble;
+        private Vector3 defaultScale;
+        
         private void Awake()
         {
-            if (dialogueWindow == null)
-                Debug.LogAssertion("dialogueWindowが未設定です。");
+            defaultScale = fuguSpeechBubble.rectTransform.localScale;
             
-            dialogueWindow.SetActive(false);
-            dialogueWindow.transform.localScale = Vector3.zero;
+            fuguSpeechBubble.rectTransform.localScale = Vector3.zero;
+            playerSpeechBubble.rectTransform.localScale = Vector3.zero;
+            
+            fuguSpeechBubble.gameObject.SetActive(false);
+            playerSpeechBubble.gameObject.SetActive(false);
         }
 
         public void Display(DialogueItem item)
@@ -34,18 +39,66 @@ namespace Module.Application.Dialogue
                 return;
             }
             
-            dialogueText.text = item.Text; 
-            characterIcon.sprite = item.characterIcon;
-            if (!dialogueWindow.activeSelf)
+            SwitchSpeechBubble(item.isFuguDialogue);
+            
+            // TODO: コルーチンで一文字ずつ表示実装    
+            PrintDialogue(item.Text); 
+            DialogueAnimation(true, item.isFuguDialogue);
+           
+        }
+        
+        /// <summary>
+        /// フグのセリフかプレイヤーのセリフかによって吹き出しの画像と位置が変わるので、切り替え可能に
+        /// </summary>
+        private void SwitchSpeechBubble(bool isFugu)
+        {
+            if (isFugu)
             {
-                dialogueWindow.SetActive(true);
-                dialogueWindow.transform.DOScale(new Vector3(maxSize, maxSize, 1f), playBackTime);
+                if (activeSpeechBubble == fuguSpeechBubble)
+                    return;
+                
+                activeSpeechBubble = fuguSpeechBubble;
+                activeText = fuguText;
+                Debug.Log("Call SwitchSpeechBubble: isFugu = " + isFugu);
+                fuguSpeechBubble.gameObject.SetActive(true);
+                playerSpeechBubble.gameObject.SetActive(false);
+            }
+            else
+            {
+                if (activeSpeechBubble == playerSpeechBubble)
+                    return;
+                
+                activeSpeechBubble = playerSpeechBubble;
+                activeText = playerText;
+                
+                fuguSpeechBubble.gameObject.SetActive(false);
+                playerSpeechBubble.gameObject.SetActive(true);
+            }
+        }
+
+        private void DialogueAnimation(bool isOpen, bool isFugu)
+        {
+            // TODO: フグのセリフは右下から左上、プレイヤーのセリフは左下から右上にアニメーションさせたい。閉じる際はその逆
+            if (isOpen)
+            {
+                if (isFugu)
+                    fuguSpeechBubble.rectTransform.DOScale(defaultScale, playBackTime);
+                else
+                    playerSpeechBubble.rectTransform.DOScale(defaultScale, playBackTime);
+            }
+            else
+            {
+                if (isFugu)
+                    fuguSpeechBubble.rectTransform.DOScale(Vector3.zero, playBackTime);
+                else
+                    playerSpeechBubble.rectTransform.DOScale(Vector3.zero, playBackTime);
             }
         }
 
         public async UniTask HideAsync(CancellationToken cancellationToken)
         {
-            dialogueWindow.transform.DOScale(Vector3.zero, playBackTime);
+            bool isFugu = GetActiveImageIsFugu();
+            DialogueAnimation(false, isFugu);
                     
             // アニメーション終了まで待機
             await UniTask.Delay(TimeSpan.FromSeconds(playBackTime), cancellationToken: cancellationToken);
@@ -53,8 +106,18 @@ namespace Module.Application.Dialogue
             // オブジェクト破棄後等でなければ実行
             if (!cancellationToken.IsCancellationRequested)
             {
-                dialogueWindow.SetActive(false);
+                activeSpeechBubble.gameObject.SetActive(false);
             }
+        }
+        
+        private void PrintDialogue(string text)
+        {
+            activeText.text = text;
+        }
+        
+        private bool GetActiveImageIsFugu()
+        {
+            return activeSpeechBubble == fuguSpeechBubble;
         }
 
     }
