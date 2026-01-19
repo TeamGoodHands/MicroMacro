e7a81b85fcd7bdbff7c22539288eb5bd81822d0a
harumas <haruto09.game@gmail.com>
2026-01-19 22:07:58 +0900

    風船ギミックバグ修正

--- MicroMacro/Assets/Scripts/Module/Gimmick/BalloonPlayerKiller.cs
+++ MicroMacro/Assets/Scripts/Module/Gimmick/BalloonPlayerKiller.cs
@@ -11,16 +11,43 @@ namespace Module.Gimmick
 
         private int balloonHealth;
         private bool balloonIsDead;
+        private HealthStatus playerHealthStatus;
+
+        private void Start()
+        {
+            balloon.OnReset += OnBalloonReset;
+            playerHealthStatus = GameObject.FindWithTag(Tag.Player).GetComponent<HealthStatus>();
+            playerHealthStatus.OnDeath += OnPlayerDeath;
+        }
+
+        private void OnPlayerDeath()
+        {
+            if (balloonIsDead)
+                return;
+
+            KillBalloon();
+        }
+
+        private void OnBalloonReset()
+        {
+            balloonIsDead = false;
+        }
 
         private void Update()
         {
-            if (balloon.HealthPoint > 0)
+            if (balloon.HealthPoint > 0 || balloonIsDead)
                 return;
 
             if (Physics.Raycast(transform.position, Vector3.down, 1.5f, -1, QueryTriggerInteraction.Ignore))
             {
-                balloon.KillBalloon().Forget();
+                KillBalloon();
             }
+        }
+
+        private void KillBalloon()
+        {
+            balloon.KillBalloon().Forget();
+            balloonIsDead = true;
         }
     }
 }
