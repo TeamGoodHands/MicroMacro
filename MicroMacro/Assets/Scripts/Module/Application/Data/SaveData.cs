5fa37d8eade358411d46fba74adc6cb18afe9f4d
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2026-01-20 00:11:52 +0900

    セーブ機能一旦統合

---
+++ MicroMacro/Assets/Scripts/Module/Application/Data/SaveData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module.Application.Data
+{
+    [Serializable]
+    public class SaveData
+    {
+        // クリア済みのステージIDリスト
+        // List<string>にすることで "1-1", "Boss", "Extra-1" など自由に増やせる
+        public List<string> clearedStageIds = new List<string>();
+
+        // 最後に遊んだ日時など（将来用）
+        public string lastPlayedDate;
+    }
+}
