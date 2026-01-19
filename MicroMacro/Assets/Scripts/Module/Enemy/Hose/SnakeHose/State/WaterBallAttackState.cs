3b635d48b4471e580fa6185b6ac54844406fc741
harumas <haruto09.game@gmail.com>
2026-01-19 19:37:50 +0900

    水道ボス修正

--- MicroMacro/Assets/Scripts/Module/Enemy/Hose/SnakeHose/State/WaterBallAttackState.cs
+++ MicroMacro/Assets/Scripts/Module/Enemy/Hose/SnakeHose/State/WaterBallAttackState.cs
@@ -46,9 +46,6 @@ namespace Module.Enemy.Hose.SnakeHose
 
         internal override void OnEnter()
         {
-            condition.CurrentState = SnakeHoseCondition.State.BeamAttack;
-            return;
-            
             CancellationTokenSource source = CancellationTokenSource.CreateLinkedTokenSource(CancellationToken, damageCanceller.Token);
             PatrolRandomlyAsync(parameter.AttackHeight, source.Token).Forget();
 
